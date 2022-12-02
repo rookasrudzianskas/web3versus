356965be99b38c27ee2c766582455890e82466a1
Rokas <rookas.rudzenskas@gmail.com>
2022-12-02 15:23:53 +0200

    Add motion to World section
    This adds motion to the World section.

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { TitleText, TypingText } from '../components';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const World = () => (
   <section>
     World section
