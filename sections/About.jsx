a1a1ac9c9110620eb9d9467eb7d57286a628ff59
Rokas <rookas.rudzenskas@gmail.com>
2022-12-02 14:32:16 +0200

    Add arrow-down image to About section
    The arrow-down image is used to indicate that the user should scroll down to
    explore the madness of the metaverse.

--- sections/About.jsx
+++ sections/About.jsx
@@ -38,8 +38,13 @@ const About = () => (
 
                 <span className="font-extrabold text-white">explore</span> the madness
                 of the metaverse by scrolling down
-
             </motion.p>
+            <motion.img
+                variants={fadeIn('up', 'tween', 0.3, 1)}
+                src="/arrow-down.svg"
+                alt="arrow-down"
+                className="w-[18px] h-[28px] object-contain mt-[28px]"
+            />
         </motion.div>
     </section>
 );
