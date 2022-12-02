8a98076e05db8a628ace7c0d495f9f7715034936
Rokas <rookas.rudzenskas@gmail.com>
2022-12-02 14:34:56 +0200

    Add Explore section
    - Add Explore section
    - Add ExploreCard component
    - Add TitleText component
    - Add TypingText component
    - Add exploreWorlds constant
    - Add staggerContainer utility

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { exploreWorlds } from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { ExploreCard, TitleText, TypingText } from '../components';
+
 const Explore = () => (
   <section>
     Explore section
