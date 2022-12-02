6d647034dbf5cb7d5199780df3aeec331f4e2814
Rokas <rookas.rudzenskas@gmail.com>
2022-12-02 16:06:56 +0200

    Add motion to footer
    - Add motion to footer
    - Add motion to feedback

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -31,6 +31,22 @@ const Feedback = () => (
                   metaverse you can use it as anything”
               </p>
           </motion.div>
+
+          <motion.div
+              variants={fadeIn('left', 'tween', 0.2, 1)}
+              className="relative flex-1 flex justify-center items-center"
+          >
+              <img src="/planet-09.png" alt="planet"
+                   className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+              />
+
+              <motion.div
+                variants={zoomIn(0.4, 1)}
+                className="lg:block hidden absolute -left-[10%] top-[3%]"
+              >
+                  <img src="/stamp.png" alt="stamp" className="w-[155px] h-[155px] object-contain"/>
+              </motion.div>
+          </motion.div>
       </motion.div>
   </section>
 );
