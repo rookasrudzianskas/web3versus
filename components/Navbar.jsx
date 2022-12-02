ed7096d4fdb9b373e7c1daebba45970d81fb1dba
Rokas <rookas.rudzenskas@gmail.com>
2022-12-02 13:54:10 +0200

    Add menu icon to navbar
    The menu icon is used to open the side menu.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,6 +22,7 @@ const Navbar = () => (
           className="w-[24px] h-[24px] object-contain"
       />
       <h2 className="font-extrabold	 text-[24px] leading-[30px] text-white">METAVERSUS</h2>
+      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain"/>
     </div>
   </motion.nav>
 );
