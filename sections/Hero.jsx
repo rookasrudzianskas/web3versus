1e45afb1a8adb94212705c16fc9a0cb41f3ffef7
Rokas <rookas.rudzenskas@gmail.com>
2022-12-02 14:07:16 +0200

    Add a new section to the homepage
    This section will be used to describe the project and its goals.

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -9,8 +9,30 @@ const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
     <motion.div
         variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex-col`}
     >
+        <div className="flex justify-center items-center flex-col relative z-10">
+            <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+                Metaverse
+            </motion.h1>
+            <motion.div
+                variants={textVariant(1.2)}
+                className="flex flex-row justify-center items-center"
+            >
+                <h1 className={styles.heroHeading}>Ma</h1>
+                <div className={styles.heroDText} />
+                <h1 className={styles.heroHeading}>Ness</h1>
+            </motion.div>
+        </div>
 
+        <motion.div
+            variants={slideIn('right', 'tween', 0.2, 1)}
+        >
+
+        </motion.div>
     </motion.div>
   </section>
 );
